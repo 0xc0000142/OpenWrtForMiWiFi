065e0ab7e0e47940e4f268a2032bb5de315b426b
0xc0000142 <52188337+0xc0000142@users.noreply.github.com>
2021-01-21 12:05:21 +0800

    Create diy-part1.sh

---
+++ config/packages/diy-part1.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
+git clone -b packages-19.07 https://github.com/liuran001/openwrt-packages package/liuran001_packages
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
