d9ba20679ba05e9ee13d7c5c5d73afe23c1f28ab
0xc0000142 <52188337+0xc0000142@users.noreply.github.com>
2021-01-22 09:29:20 +0800

    Update diy-part2.sh

--- config/packages-k5.4/diy-part2.sh
+++ config/packages-k5.4/diy-part2.sh
@@ -22,4 +22,6 @@ mkdir -p parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O luci-app-diskman/Makefile
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O parted/Makefile
 rm -rf package/feeds/packages/https-dns-proxy/
+rm -rf feeds/packages/net/https-dns-proxy
+rm -rf feeds/packages/https-dns-proxy/
 rm -rf package/feeds/packages/btrfs-progs/
